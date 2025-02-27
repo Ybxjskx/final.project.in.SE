94e635ec8ee441987037c78ac0eb51b5ab906bef
Coder-Artist <52375055+LeafGreenRemake@users.noreply.github.com>
2025-02-27 19:25:58 +0200

    Update HelloApplication.java

--- src/main/java/org/example/finalproject1/HelloApplication.java
+++ src/main/java/org/example/finalproject1/HelloApplication.java
@@ -7,12 +7,14 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException
     {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        // creating a title
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
